04553a04bf020841e241597689b0d21c09151a7d
bhatshakran <bhatshakran@gmail.com>
2022-11-01 15:06:43 +0530

    Completed features

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,6 +3,11 @@ import Container from './Container';
 import HandshakeIcon from '@mui/icons-material/Handshake';
 import GppGoodIcon from '@mui/icons-material/GppGood';
 import SpaIcon from '@mui/icons-material/Spa';
+import PoolIcon from '@mui/icons-material/Pool';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import WineBarIcon from '@mui/icons-material/WineBar';
+import PhishingIcon from '@mui/icons-material/Phishing';
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
 
 const Features: React.FC = () => {
   return (
@@ -64,6 +69,56 @@ const Features: React.FC = () => {
           </div>
         </div>
       </Container>
+      <div className='bg-black'>
+        <Container>
+          <div className=' w-full flex flex-wrap items-center justify-between p-8'>
+            <div className='w-full md:w-1/2 p-8'>
+              <div className='border  border-white border-opacity-60 pt-2 pl-2'>
+                <img
+                  src='https://res.cloudinary.com/tiny-house/image/upload/v1560641327/mock/Dubai/dubai-listing-2_qc2kos.jpg'
+                  alt=''
+                />
+              </div>
+            </div>
+            <div className='w-full md:w-1/2 text-white p-8'>
+              <h3 className='text-5xl font-regis'>
+                You'll never want to leave
+              </h3>
+              <p className='mt-16 font-silka text-lg'>
+                The freedom to do exactly what you want" whether it is dipping
+                into a tub of hot water with a book or exploring local culture.
+                The perfect place to open your heart and let it decide!
+              </p>
+              <ul className='mt-16 text-2xl font-silka flex flex-wrap w-full gap-y-4 text-secondary items-center'>
+                <li className='w-1/2 flex items-center '>
+                  <SpaIcon fontSize='large' />
+                  <p className='text-white'> Spa</p>
+                </li>
+                <li className='w-1/2 flex items-center'>
+                  <PoolIcon fontSize='large' />
+                  <p className='text-white'> Pool</p>
+                </li>
+                <li className='w-1/2 flex items-center'>
+                  <RestaurantMenuIcon fontSize='large' />
+                  <p className='text-white'> Dining</p>
+                </li>
+                <li className='w-1/2 flex items-center'>
+                  <WineBarIcon fontSize='large' />
+                  <p className='text-white'> 24/7 Bar</p>
+                </li>
+                <li className='w-1/2 flex items-center'>
+                  <PhishingIcon fontSize='large' />
+                  <p className='text-white'> Fishing</p>
+                </li>
+                <li className='w-1/2 flex items-center'>
+                  <OutdoorGrillIcon fontSize='large' />
+                  <p className='text-white'> Barbeque</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
