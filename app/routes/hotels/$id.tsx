974d0eec30ee2fce60734f8acc2375677f06aa47
bhatshakran <bhatshakran@gmail.com>
2022-11-09 21:57:59 +0530

    Changed hotel page layout

--- app/routes/hotels/$id.tsx
+++ app/routes/hotels/$id.tsx
@@ -5,10 +5,7 @@ import type { LoaderFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import Arrow from '~/components/Arrow';
 import Container from '~/components/Container';
-import ImageIcon from '@mui/icons-material/Image';
 import PlaceIcon from '@mui/icons-material/Place';
-import { getCurrentBreakpoint } from '~/lib/utils/getCurrentBreakpoint';
-import React from 'react';
 import Header from '~/components/Header';
 
 export const loader: LoaderFunction = async ({ request, params }) => {
@@ -41,109 +38,67 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 const Hotel = () => {
   const { hotel } = useLoaderData();
 
-  const addOrRemoveImgEvtListener = React.useCallback(() => {
-    const breakpointValue = getCurrentBreakpoint();
-    console.log(breakpointValue);
-    const img = document.querySelector('.hotel-img');
-    if (breakpointValue === 'sm' || breakpointValue === undefined) {
-      img?.removeEventListener('mouseenter', zoomImg);
-      img?.removeEventListener('mouseleave', unzoomImg);
-    } else {
-      img?.addEventListener('mouseenter', zoomImg);
-      img?.addEventListener('mouseleave', unzoomImg);
-    }
-  }, []);
-
-  React.useEffect(() => {
-    addOrRemoveImgEvtListener();
-    window.addEventListener('resize', addOrRemoveImgEvtListener);
-    return () =>
-      window.removeEventListener('resize', addOrRemoveImgEvtListener);
-  }, [addOrRemoveImgEvtListener]);
-
-  const zoomImg = () => {
-    const img = document.querySelector('.hotel-img');
-    if (img) {
-      const image = img as HTMLImageElement;
-      image.style.opacity = '1';
-      image.style.scale = '3';
-      image.style.transition = 'all 0.25s ease-in';
-    }
-  };
-
-  const unzoomImg = () => {
-    const img = document.querySelector('.hotel-img');
-    if (img) {
-      const image = img as HTMLImageElement;
-      image.style.opacity = '0.8';
-      image.style.scale = '1';
-    }
-  };
-
   return (
-    <main className=' bg-backgroundColor min-h-screen overflow-hidden flex justify-center  px-8  md:px-0 md:py-0'>
+    <main className=' bg-backgroundColor min-h-screen overflow-hidden flex justify-center  px-8 md:py-0'>
       <Container>
         <Header />
-        <div className='flex flex-col justify-start md:justify-center  items-center h-full my-24 md:my-0 '>
-          <div className='font-silka  items-center gap-2 hidden md:flex '>
-            <h6 className='text-xs '>Hover on the image to zoom </h6>
-            <ImageIcon />
-          </div>
-          <div className=' flex flex-wrap justify-evenly  items-center w-full h-1/2 gap-10'>
-            <div className='w-full md:w-1/4 lg:w-1/4 flex flex-col gap-4 md:gap-2 font-silka'>
-              <h2 className=' text-3xl  w-full text-black font-regis '>
-                {hotel.title}
-              </h2>
-              <h3 className=' text-blue-700'>
-                {' '}
-                <PlaceIcon /> {hotel.address}
-              </h3>
-
-              <h3 className='font-bold bg-black text-white p-2 rounded-md'>
-                Price: ${hotel.price}
-              </h3>
-              <h3 className='font-bold'>Guests: {hotel.numOfGuest}</h3>
-              <h3 className='font-bold'>
-                City & Country: {hotel.city}, {hotel.country}
-              </h3>
-            </div>
-
-            <div className='w-full md:w-1/4 lg:w-1/4 '>
+        <div className='flex flex-col justify-start items-center h-full my-24 '>
+          <div className=' flex flex-wrap justify-between  items-start w-full h-1/2 gap-10'>
+            <div className='w-full md:w-1/2  '>
               <img
                 src={hotel.image}
                 alt=''
-                className='hotel-img w-full h-full rounded-md cursor-pointer opacity-100 md:opacity-80'
+                className='hotel-img w-full h-full rounded-md cursor-pointer'
               />
             </div>
-            <div className='w-full md:w-1/4 lg:w-1/4 flex flex-col gap-8  overflow-hidden'>
-              <div className='font-silka flex flex-col gap-6 w-full'>
-                <div className='flex flex-col'>
-                  <label htmlFor='checkin' className=' font-bold'>
-                    Check in date:
-                  </label>
-                  <input
-                    type='datetime-local'
-                    name='checkinpicker'
-                    id='checkinpicker'
-                    className='focus:outline-none cursor-pointer px-2 py-1 rounded-full border border-lightorange'
-                  />
-                </div>
-                <div className='flex flex-col'>
-                  <label htmlFor='checkout' className=' font-bold'>
-                    Check out date:
-                  </label>
-                  <input
-                    type='datetime-local'
-                    name='checkoutpicker'
-                    id='checkoutpicker'
-                    className='focus:outline-none cursor-pointer px-2 py-1 rounded-full border border-lightorange'
-                  />
-                </div>
+            <div className='w-full md:w-1/3  flex flex-col gap-16 font-silka'>
+              <div className='flex flex-col gap-4 '>
+                <h2 className=' text-3xl  w-full text-black font-regis '>
+                  {hotel.title}
+                </h2>
+                <h3 className=' text-blue-700'>
+                  {' '}
+                  <PlaceIcon /> {hotel.address}
+                </h3>
+
+                <h3 className='font-bold bg-black text-white p-2 rounded-md'>
+                  Price: ${hotel.price}
+                </h3>
+                <h3 className='font-bold'>Guests: {hotel.numOfGuest}</h3>
+                <h3 className='font-bold'>
+                  City & Country: {hotel.city}, {hotel.country}
+                </h3>
               </div>
-              <button className='  flex items-center justify-center gap-2 p-2 rounded-full font-silka bg-lightorange text-white'>
-                Request to book
-                <Arrow />
-              </button>
+              <div className='w-full  ml-auto  flex flex-col gap-8  overflow-hidden'>
+                <div className='font-silka flex flex-col gap-6 w-full'>
+                  <div className='flex flex-col'>
+                    <label htmlFor='checkin' className=' font-bold'>
+                      Check in date:
+                    </label>
+                    <input
+                      type='datetime-local'
+                      name='checkinpicker'
+                      id='checkinpicker'
+                      className='focus:outline-none cursor-pointer px-2 py-1 rounded-full border border-lightorange'
+                    />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label htmlFor='checkout' className=' font-bold'>
+                      Check out date:
+                    </label>
+                    <input
+                      type='datetime-local'
+                      name='checkoutpicker'
+                      id='checkoutpicker'
+                      className='focus:outline-none cursor-pointer px-2 py-1 rounded-full border border-lightorange'
+                    />
+                  </div>
+                </div>
+                <button className='  flex items-center justify-center gap-2 p-2 rounded-full font-silka bg-lightorange text-white'>
+                  Request to book
+                  <Arrow />
+                </button>
+              </div>
             </div>
           </div>
         </div>
