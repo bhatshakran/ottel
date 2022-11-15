3c57f5f5a44d9cdca163f9284ff37b44ad05da11
bhatshakran <bhatshakran@gmail.com>
2022-11-15 12:47:09 +0530

    fix: server dependencies issue

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverDependenciesToBundle: [
+    /^@apollo\/client.*/,
+    /^ts-invariant.*/,
+    /^zen-observable-ts.*/,
+  ],
 };
