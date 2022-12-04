0099075f8dee8c526b37dda5ccb0282851c227d0
bhatshakran <bhatshakran@gmail.com>
2022-12-04 11:57:19 +0530

    Fixed name and favicon issue

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Ottelo | Hotels',
   viewport: 'width=device-width,initial-scale=1',
 });
 
